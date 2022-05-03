98f08c33b017040412e2fd27429b6b97418c22e2
snzmxm <mxmdev96@gmail.com>
2022-05-03 13:10:21 +0700

    Navigation controller

--- SetitngsUiKit/ViewController.swift
+++ SetitngsUiKit/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupNavigationController()
+        view.backgroundColor = .white
+    }
 
+    //Метод, который реализует навигейшен контроллер
+    private func setupNavigationController() {
+        navigationItem.title = "Настройки"
+
+        //Цвет фона
+        let apperance = UINavigationBarAppearance()
+        apperance.configureWithOpaqueBackground()
+        apperance.backgroundColor = .white
+
+        //Цвет текста
+        let titleAttribute  = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.black]
+        apperance.titleTextAttributes = titleAttribute
+        
+
+        navigationController?.navigationBar.standardAppearance = apperance
+        navigationController?.navigationBar.scrollEdgeAppearance = apperance
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
 
