030da3f52c6f4fa3db27de3ebd68a50f9cadcbae
snzmxm <mxmdev96@gmail.com>
2022-05-03 12:05:55 +0700

    Initial Commit

---
+++ SetitngsUiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SetitngsUiKit
+//
+//  Created by SNZ on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
